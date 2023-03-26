068e25b8d6b90309481c5569454caada281331f3
SQuazar <flawesv@yandex.ru>
2023-03-26 21:15:14 +0300

    Added Dotenv for support .env file

--- src/main/java/net/quazar/discord/Main.java
+++ src/main/java/net/quazar/discord/Main.java
@@ -1,9 +1,12 @@
 package net.quazar.discord;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main {
     public static void main(String[] args) {
-        String token = System.getenv("BOT_TOKEN");
-        String webhookUrl = System.getenv("WEBHOOK_URL");
+        var env = Dotenv.load();
+        String token = env.get("BOT_TOKEN");
+        String webhookUrl = env.get("WEBHOOK_URL");
         new ParodyBot(token, webhookUrl).run();
     }
 }
